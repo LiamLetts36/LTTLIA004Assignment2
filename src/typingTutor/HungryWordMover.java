23c175e6c13854f1ea6dfef1bf6e2a0e1fc3635d
Liam <liam-letts@hotmail.com>
2022-08-26 00:23:06 +0200

    Git Commit 2 -Initiated Variables and Constructor for HungryWordMover class

--- src/typingTutor/HungryWordMover.java
+++ src/typingTutor/HungryWordMover.java
@@ -1,4 +1,25 @@
 package typingTutor;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class HungryWordMover {
+    private FallingWord myWord;
+    private AtomicBoolean done;
+    private AtomicBoolean pause;
+    private Score score;
+    CountDownLatch startLatch;
+
+    HungryWordMover( FallingWord word,WordDictionary dict, Score score,
+               CountDownLatch startLatch, AtomicBoolean d, AtomicBoolean p) {
+        this(word);
+        this.startLatch = startLatch;
+        this.score=score;
+        this.done=d;
+        this.pause=p;
+    }
+
+
+    public HungryWordMover(FallingWord word) {
+    }
 }
